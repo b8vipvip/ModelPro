8b81a1bdc8a8c103fd358b5cbffe4750bc8f7053
b8vipvip <917931227@qq.com>
2026-09-24 16:45:14 +0800

    test: align single auto-export ownership (v0.1.29)

--- extension/background.js
+++ extension/background.js
@@ -31,7 +31,7 @@ import {
 } from './tab-feature-runtime.js';
 import { ACCOUNT_REFRESH_ALARM } from './account-refresh-scheduler.js';
 
-const RUNTIME_CODE_VERSION = '0.1.28';
+const RUNTIME_CODE_VERSION = '0.1.29';
 const NATIVE_HOST = 'com.gptlock.core';
 const RECONNECT_ALARM = 'gptlock-native-reconnect';
 const REQUEST_TIMEOUT_MS = 7000;
