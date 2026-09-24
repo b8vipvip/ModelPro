2b5fbcae5ed9185b8e66004689c6be14a342e922
b8vipvip <917931227@qq.com>
2026-09-24 14:31:12 +0800

    test: remove obsolete Work-turn expectation (v0.1.15)

--- extension/background.js
+++ extension/background.js
@@ -31,7 +31,7 @@ import {
 } from './tab-feature-runtime.js';
 import { ACCOUNT_REFRESH_ALARM } from './account-refresh-scheduler.js';
 
-const RUNTIME_CODE_VERSION = '0.1.14';
+const RUNTIME_CODE_VERSION = '0.1.15';
 const NATIVE_HOST = 'com.gptlock.core';
 const RECONNECT_ALARM = 'gptlock-native-reconnect';
 const REQUEST_TIMEOUT_MS = 7000;
